2eaf43370b2e6fe4456f7c8f5874ab6daaec4b1b
MaxNeu2411 <max.neuland@gmail.com>
2026-08-31 20:17:04 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import { queryClientInstance } from "@/lib/query-client";
 import {  HashRouter, Route, Routes } from "react-router-dom";
 
 import PageNotFound from "./lib/PageNotFound";
-import Layout from "@/components/Layout";
-
 import NewOrder from "@/pages/NewOrder";
 import MenuPage from "@/pages/MenuPage";
 
